e5873e53c467cce79dc0aedeff000ec9ae1ce0ac
Vivek Tyagi <tyagiviv@gmail.com>
2024-12-22 13:27:38 +0200

    adding changes to remove from fav & fixing fav page

--- details/views.py
+++ details/views.py
@@ -12,22 +12,22 @@ def poster_details(request, id):
         return render(request, 'poster_details.html', {'message': 'Poster not found'})
 
     # Adding section to check if poster is already fav or not and based on that show button add to fav
-    is_favorite = False
-    try:
-        Favorite.objects.get(poster_id_id=poster)
-        is_favorite = True
-    except Favorite.DoesNotExist:
-        is_favorite = False
+    is_favorite = Favorite.objects.filter(poster_id_id=poster.id).exists()
 
+    # Debugging: Print the value of is_favorite
+    print(f"Is favorite: {is_favorite}")
 
     if request.method == 'POST':
-        try:
+        if is_favorite:
+            # Remove favorites
             favorite = Favorite.objects.get(poster_id_id=poster.id)
-
-        except Favorite.DoesNotExist:
+            favorite.delete()
+            return redirect('favorite')
+        else:
+            # Add favorites
             Favorite.objects.create(poster_id_id=poster.id)
+            return redirect('favorite')
 
-        return redirect('favorite')
 
 
-    return render(request, 'poster_details.html', {'poster': poster})
+    return render(request, 'poster_details.html', {'poster': poster, 'is_favorite': is_favorite})
